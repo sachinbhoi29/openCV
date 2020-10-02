5427d1e58c50525d467f31528dd5f95246b68fb5
Sachin Bhoi <sachin.bhoi@exponentia.ai>
2020-10-02 17:50:03 +0530

    webcam color masks

--- Webcam_Trackbar_mask.py
+++ Webcam_Trackbar_mask.py
@@ -7,12 +7,12 @@ cap.set(3,640) #width id is 3
 cap.set(4,480) #height id is 4
 cap.set(10,100) #brightness id is 10
 
-#Trackbar function
+#Trackbar 
 def empty(a):
     pass 
 cv2.namedWindow("TrackBars")
 cv2.resizeWindow("TrackBars",640,240)
-cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty) # name of the bar, TrackBar, min, max, function
 cv2.createTrackbar("Hue Max","TrackBars",179,179,empty)
 cv2.createTrackbar("Sat Min","TrackBars",0,255,empty)
 cv2.createTrackbar("Sat Max","TrackBars",255,255,empty)
@@ -20,7 +20,7 @@ cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
 cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
 
 while True:
-    _,img = cap.read()
+    _,img = cap.read()    # first parameter True or False and the second parameter set of images which forms a video
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
     h_max = cv2.getTrackbarPos("Hue Max","TrackBars")
@@ -36,7 +36,3 @@ while True:
     cv2.imshow("mask",mask)
     if cv2.waitKey(10) & 0xFF==ord('q'):
         break
-
-
-
-# def findColor(img):
---
+++ Webcam_color_mask.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3,frameWidth) # (ID,argument)
+cap.set(4,frameHeight) # (height ID, frameHeight)
+cap.set(10,150) # beightness reduce 150
+
+# mask colour values of hue, sat, and val
+myColors = [[5,107,0,19,225,225],                
+            [133,56,0,159,159,255],
+            [57,76,0,100,255,255]]
+
+def findColor(img,myColors):
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV,lower,upper)
+        cv2.imshow(str(color[0]),mask)
+
+while True:
+    success, img = cap.read()
+    findColor(img, myColors)
+    cv2.imshow("Result",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
